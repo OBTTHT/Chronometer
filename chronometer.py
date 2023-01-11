35f2a930cdd3e32ac4d696da5d7e93070ffbbdc5
OBTTHT <95370738+OBTTHT@users.noreply.github.com>
2023-01-11 18:49:52 +0300

    Add files via upload

---
+++ chronometer.py
@@ -0,0 +1,66 @@
+from cgitb import reset
+import tkinter as Tkinter
+from datetime import datetime
+from tracemalloc import stop, start
+
+counter = 0
+running = False
+
+name = str(input("Adınızı Giriniz: "))
+
+def counterlabel(label):
+    def count():
+        if running:
+            global counter
+            if counter == 0:
+                display = f"Hoşgeldin {name}"
+                # before ::
+                display = 'Hazır!'
+                # after ::
+                display = '@obttht'
+                # and running ;
+            else:
+                tt = datetime.utcfromtimestamp(counter)
+                string = tt.strftime('%H:%M:%S')
+                display = string
+
+            label['text'] = display
+
+            label.after(1000, count)
+            counter += 1 
+
+    count()
+
+def Start(label):
+    global running
+    running = True
+    counterlabel(label)
+    start['state'] = 'disabled'
+    stop['state'] = 'normal'
+    reset['state'] = 'normal'
+
+def Reset(label):
+    global counter
+    counter = 0
+    if not running:
+        reset['state'] = 'disabled'
+        label['text'] = '00:00:00'
+    else:
+        label['text'] = '00:00:00'
+
+root = Tkinter.Tk()
+root.title("Kronometre")
+renk = input("Renk Seçiniz: ")
+
+root.minsize(width=250, height=70)
+label = Tkinter.Label(root, text='Hazır!', fg=renk, font='Verdana 40 bold')
+label.pack()
+f = Tkinter.Frame(root)
+start = Tkinter.Button(f, text='Başla', width=6, command=lambda: Start(label))
+stop = Tkinter.Button(f, text='Durdur', width=6, state='disabled', command=stop)
+reset = Tkinter.Button(f, text='Sıfırla', width=6, state='disabled', command=lambda: Reset(label))
+f.pack(anchor='center', pady=5)
+start.pack(side='left')
+stop.pack(side='left')
+reset.pack(side='left')
+root.mainloop()
